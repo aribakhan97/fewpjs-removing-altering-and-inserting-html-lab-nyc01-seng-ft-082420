40f05b3de14ac2bb01172aa9a817c91a278205a6
aribakhan97 <akhan41@binghamton.edu>
2020-10-07 17:21:21 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const testVar = "heyy"
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ariba is the champion!";
+newHeader.classname = "victory";
